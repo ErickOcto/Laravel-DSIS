1436a35cda55462be6dd9d04f226a4952f8f7940
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2024-01-03 11:42:23 +0800

    layouting

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,39 @@
+<div class="container-fluid" style="background:#165690">
+    <div class="container">
+        <div class="row pt-48 pb-48">
+            <div class="col-6">
+                <div class="title-3-pth mb-16">
+                    Tentang Kami
+                </div>
+                <div class="copy" style="color: white">
+                    Sekolah Kejuruan di Surabaya, Jawa Timur yang berlokasi di Jl. Smea No. 4, Wonokromo Surabaya, SMK Negeri 1 Surabaya bertekad mencapai perbaikan yang berkesinambungan berdasarkan sistem manajemen mutu ISO 9001:2008. Telp. 031-8292038 FAX. 031- 8292039, Email : info@smkn1-sby.sch.id
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="title-3-pth mb-16">
+                    Link
+                </div>
+                <div class="copy" style="color: white
+                ">
+                    <ul>
+                        <li>
+                            BKK
+                        </li>
+                        <li>
+                            LSP
+                        </li>
+                        <li>
+                            PPDB
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="title-3-pth mb-16">
+                    Lokasi SMK6 Balikpapan
+                </div>
+                <iframe class="w-full" src="{{ url('https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1994.4687611622726!2d116.8775678!3d-1.2039466!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2df148b833fd9381%3A0x33264090252054aa!2sSMK%20Negeri%206%20Balikpapan!5e0!3m2!1sid!2sid!4v1704250437013!5m2!1sid!2sid') }}" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,87 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container">
+    <img src="/image/logo-smk1sby.png" alt="" class="navbar-brand" style="max-width: 200px">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mb-2 mb-lg-0 ms-auto">
+        <li class="nav-item mx-2">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item mx-2 dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Profil
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Visi & Misi</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Struktur & Organisasi</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Guru dan Tenaga Pendidikan</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Sarana & Prasarana</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Teaching Factory</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Gallery Foto</a></li>
+          </ul>
+        </li>
+
+        <li class="nav-item mx-2 dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Jurusan
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Akuntansi dan Keuangan Lembaga</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Bisnis daring dan Pemasaran</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Rekayasa Perangkat Lunak</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Multimedia</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Teknik Komputer dan Jaringan</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Desain Komunikasi Visual</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Perhotelan</a></li>
+          </ul>
+        </li>
+
+        <li class="nav-item mx-2 dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Informasi
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Mr. Iso</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Informasi</a></li>
+          </ul>
+        </li>
+
+        <li class="nav-item mx-2">
+          <a class="nav-link" aria-current="page" href="#">Berita</a>
+        </li>
+        <li class="nav-item mx-2">
+          <a class="nav-link" aria-current="page" href="#">Modul</a>
+        </li>
+
+        <li class="nav-item mx-2 dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Program
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Lembaga Sertifikasi Profesi</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Bursa Kerja Khusus</a></li>
+          </ul>
+        </li>
+
+        <li class="nav-item mx-2">
+          <a class="nav-link" aria-current="page" href="{{ route('login') }}">Login</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
