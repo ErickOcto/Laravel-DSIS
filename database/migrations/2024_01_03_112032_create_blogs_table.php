3dd1a046389f6c3006165970b53e55fdd1bec52e
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2024-01-03 19:43:29 +0800

    buat migration blog dan kontroller, serta update user table

---
+++ database/migrations/2024_01_03_112032_create_blogs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->string('photo');
+            $table->string('slug');
+            $table->text('konten');
+            $table->foreignId('user_id');
+            $table->foreignId('category_id');
+            $table->integer('lihat')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
