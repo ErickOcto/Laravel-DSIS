fb8c00bcb31997b0f31db0ab2470f1aad2276d95
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2024-01-03 19:44:03 +0800

    slicing improve

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.admin')
+
+@section('admin-title')
+Admin Dashboard
+@endsection
+
+@section('admin-header')
+Admin Dashboard
+@endsection
+
+@section('admin-content')
+Tes
+@endsection
