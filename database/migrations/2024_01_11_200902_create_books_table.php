96bf5777a52bf05880f58868917e3dd68841e03d
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2024-01-11 20:22:52 +0800

    add kode buku to books_table

--- database/migrations/2024_01_11_200902_create_books_table.php
+++ database/migrations/2024_01_11_200902_create_books_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->integer('kode_buku');
             $table->string('author');
             $table->foreignId('book_category_id');
             $table->integer('stock');
