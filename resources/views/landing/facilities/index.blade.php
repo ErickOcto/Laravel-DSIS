126e2f5cda340afc1db08ffa1326744990828c3a
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2024-01-14 18:12:35 +0800

    add assets and update facilities landing pages

--- resources/views/landing/facilities/index.blade.php
+++ resources/views/landing/facilities/index.blade.php
@@ -5,5 +5,21 @@ Profile - Fasilitas
 @endsection
 
 @section('latihan-content')
-Tes
+
+
+<section class="section py-100 bg-primary">
+    <div class="container pt-100">
+        <div class="row">
+            <div class="col-6 d-flex align-items-center justify-content-center">
+                <div class="title text-center">
+                    Fasilitas terbaik dari kami untuk anda
+                </div>
+            </div>
+            <div class="col-6">
+                <img src="/image/main.png" alt="" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</section>
+
 @endsection
